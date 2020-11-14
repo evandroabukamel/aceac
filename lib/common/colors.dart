ff3e74e8c953065e2b2505ea76b9cd4e8cc3a124
Evandro Abu Kamel <evandro.costa@maxmilhas.com.br>
2020-11-13 23:24:16 -0300

    Removing unused colours..

--- lib/common/colors.dart
+++ lib/common/colors.dart
@@ -6,15 +6,8 @@ class AppColors {
   static const Color mainGreen = Color.fromRGBO(0, 183, 161, 1);
   static const Color secondGreen = Color.fromRGBO(0, 200, 187, 1);
 
-  static const Color cardGreyText = Color.fromRGBO(150, 150, 150, 1);
   static const Color cardWhiteText = Color.fromRGBO(242, 242, 242, 1);
   static const Color cardBlackText = Color.fromRGBO(38, 38, 38, 1);
   static const Color cardWhiteBg = Color.fromRGBO(242, 242, 242, 1);
   static const Color cardBlackBg = Color.fromRGBO(0, 113, 96, 1);
-
-  static const Color inputHintText = Color.fromRGBO(122, 122, 122, 1);
-  static const Color disabledButtonLabel = Color.fromRGBO(81, 81, 81, 1);
-  static const Color disabledButtonBackground = Color.fromRGBO(38, 38, 38, 1);
-
-  static const Color buttonBackground = Colors.white;
 }
